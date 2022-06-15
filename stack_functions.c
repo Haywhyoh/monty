8b6bde4e94c31dce1b874b311e928e1bbd1791f9
Haywhyoh <adefeyisayo998@gmail.com>
2022-06-15 05:55:35 -0700

    created monty.h

---
+++ stack_functions.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * _push - insert int into a stack
+ * @stack: linked list node for stack
+ * @line_number: current line of opcode
+ *
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node;
+
+
+	new_node = malloc(sizeof(stack_t);
+	if (!new_node)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+}
