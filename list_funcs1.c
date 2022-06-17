a62892a1a35ada153d5dd710acf139824e89eab2
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-06-16 21:27:32 -0700

    final merge

--- list_funcs1.c
+++ list_funcs1.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-/**
+/**.
  * dlistint_len - returns the number of nodes in a doubly linked list
  * @h: pointer to the list
  *
---
+++ monty_functions.c
@@ -0,0 +1,1 @@
+void open_monty(char file_name,
